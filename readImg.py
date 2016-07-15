e302e9aded5f5966e2e159c0575cfb4af0d7af75
root <root@sjs_28_27.(none)>
2016-07-15 17:57:06 +0800

    del 3 files.

--- readImg.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from scipy.misc import imread, imshow
-img = imread("cat.jpg")
-print img.shape
-for i in range(0,len(img)):
-	print img[i]
-#    for j in range(0,len(img[i])):
-#        print img[i][j]
